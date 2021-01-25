0e485fa6d78c4c70e2ab910efd200c75fb87068c
MrSpy42 <nikiforos007gr@gmail.com>
2021-01-25 20:08:10 +0200

    cpu monitor update

--- src/main/java/io/github/MrSpy42/spyplugin/SpyPlugin.java
+++ src/main/java/io/github/MrSpy42/spyplugin/SpyPlugin.java
@@ -7,7 +7,7 @@ import java.lang.management.ManagementFactory;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitScheduler;
-import java.lang.management.OperatingSystemMXBean;
+import com.sun.management.OperatingSystemMXBean;
 
 
 public final class SpyPlugin extends JavaPlugin {
@@ -29,11 +29,11 @@ public final class SpyPlugin extends JavaPlugin {
         scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
-            	if(osBean.getSystemLoadAverage() > 0.97) {
-            		getServer().broadcastMessage("[SpyPlugin] Server is overloaded! Server restart is recommended.");
+            	if(osBean.getProcessCpuLoad() > 0.97) {
+            		getServer().broadcastMessage("[SpyPlugin] Server is overloaded!");
             	}
             	if(getUsedMemory() > (getTotalMemory() - 200L)) {
-            		getServer().broadcastMessage("[SpyPlugin] Resources are low! Server restart is recommended.");
+            		getServer().broadcastMessage("[SpyPlugin] Server is running out of memory!");
             	}
             }
         }, 1L, 20L);
@@ -48,12 +48,12 @@ public final class SpyPlugin extends JavaPlugin {
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    	if (cmd.getName().equalsIgnoreCase("serverload")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
-    		sender.sendMessage("[SpyPlugin] Server load is at " + String.valueOf(osBean.getSystemLoadAverage() * 100) + "%");
+    	if (cmd.getName().equalsIgnoreCase("serverload")) {
+    		sender.sendMessage("[SpyPlugin] Server load is at " + String.valueOf(osBean.getProcessCpuLoad() * 100) + "%");
     		return true;
     	} 
     	if (cmd.getName().equalsIgnoreCase("memuse")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
-    		sender.sendMessage("[SpyPlugin]" + String.valueOf(getUsedMemory()) + "MB/" + String.valueOf(getTotalMemory()) + "MB is used.");
+    		sender.sendMessage("[SpyPlugin] " + String.valueOf(getUsedMemory()) + "MB/" + String.valueOf(getTotalMemory()) + "MB is used.");
     		return true;
     	} 
     	if (cmd.getName().equalsIgnoreCase("aboutspy")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
